757b83a8449bbb4041246219b5a6ee366e0bb72a
sqmw <1951918362@qq.com>
2023-06-03 11:45:02 +0800

    old: update insert sort

--- src/my_sort/insert_sort.py
+++ src/my_sort/insert_sort.py
@@ -2,6 +2,7 @@
 import random
 
 """插入排序就是每次取出一个插入到有序的序列中"""
+"""第一次实现的插入排序是先找到位置，在进行插入后面进行了修改，在查找位置的时候就进行修改"""
 """都是升序排序"""
 
 
@@ -9,23 +10,23 @@ def insert_sort(arr):
     length = len(arr)
     # 依次取出 [1, n)
     for index_after in range(1, length):
-        move_val = arr[index_after]
-        # 找到现在需要插入的这个数字的位置
-        set_pos = 0
-        for index_before in range(0, index_after):
-            # 表示取出来的数字比第一个数字还小
-            if arr[index_after] < arr[0]:
-                set_pos = 0
-            # 表示找到了
-            elif (arr[index_after] >= arr[index_before] and index_before + 1 >= index_after) or (
-                    arr[index_before] <= arr[index_after] < arr[index_before + 1]):
-                set_pos = index_before + 1
-        for k in range(0, index_after - set_pos):
-            arr[index_after - k] = arr[index_after - k - 1]
-        arr[set_pos] = move_val
+        j = index_after - 1
+        # swap_index 一直指向的是 取出来的那个数字的下标
+        swap_index = index_after
+        while j >= 0:
+            # 设置while的跳出条件，避免while一条路走到底
+            if arr[j] <= arr[swap_index]:
+                break
+            # 表示的是需要插入的的那个数字要小一点，就交换位置
+            if arr[j] > arr[swap_index]:
+                t = arr[swap_index]
+                arr[swap_index] = arr[j]
+                arr[j] = t
+                swap_index -= 1
+            j -= 1
 
 
 if __name__ == "__main__":
-    a = [random.randint(1, 100000) for x in range(1, 100)]
+    a = [random.randint(1, 100000) for x in range(1, 10000)]
     insert_sort(a)
     print(a)
